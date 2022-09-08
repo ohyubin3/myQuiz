575b925392f280aa3e3f6fe48f8cdd75b6f324a1
Sang Oh <baul3oh@gmail.com>
2022-09-08 09:50:41 -0500

    set up done

---
+++ script.js
@@ -0,0 +1,58 @@
+let questions = [
+    {question: "1", choices: "This is where the choices will go"},
+    {question: "2", choices: "This is where the choices will go"},
+    {question: "3", choices: "This is where the choices will go"},
+    {question: "4", choices: "This is where the choices will go"},
+]
+
+// Regarding QUestions
+let questionEl = document.querySelector("h1")
+let choiceEl = document.querySelector("h2")
+let container = document.querySelector(".container")
+let currentQuestionIndex = 0;
+
+// Regarding Timer
+let timerEl = document.querySelector("h3")
+let intervalId;
+let timer = 60;
+let smallTimer = 10;
+
+
+
+renderQuestion();
+
+startTimer();
+
+
+
+
+container.addEventListener("click", function() {
+    currentQuestionIndex++;
+    renderQuestion();
+    
+    
+})
+
+function renderQuestion() {
+    questionEl.textContent = questions[currentQuestionIndex].question;
+    choiceEl.textContent = questions[currentQuestionIndex].choices;
+
+}
+
+
+
+
+function startTimer() {
+    intervalId = setInterval(function(){
+        timer--;
+        timerEl.textContent = timer;
+
+        if(timer === 0){
+            clearInterval(intervalId)
+        }
+
+
+    },1000);
+
+    
+}
